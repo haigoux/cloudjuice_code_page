b109b2874ead6182791d4368c8604b62981ac0b5
HAiGOU <73267715+lardazz222@users.noreply.github.com>
2025-02-19 16:57:08 -0600

    update Vite configuration for production base path and remove Vue config file

--- vite.config.js
+++ vite.config.js
@@ -17,4 +17,6 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  base: process.env.NODE_ENV === 'production' ? '/cloudjuice_code_page/' : '/',
+
 })
--- vue.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/cloudjuice_code_page/" : "/",
-};
